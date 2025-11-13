e1a1934d6db13f1c21229f1f3308e1abd06a90bb
Harini10092004 <harisri02022003@gmail.com>
2025-11-13 12:12:16 +0530

    commit

--- PlacementService/src/main/java/com/tnsif/PlacementService/config/AppConfig.java
+++ PlacementService/src/main/java/com/tnsif/PlacementService/config/AppConfig.java
@@ -1,8 +1,25 @@
 package com.tnsif.PlacementService.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = "http://localhost:3000")  // React connection
 public class AppConfig {
-    // You can define Beans or CORS config here if needed
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
